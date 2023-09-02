a55840f73c95d06eb38d41d5b2d4940f8c15a254
zach <zachcelona@gmail.com>
2023-09-03 06:11:40 +1200

    Restoring RoomController.java & room.fxml to base

--- src/main/java/nz/ac/auckland/se206/controllers/RoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/RoomController.java
@@ -1,18 +1,10 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
-
-import javafx.animation.AnimationTimer;
-import javafx.beans.binding.BooleanBinding;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.scene.input.KeyCode;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Box;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
@@ -21,23 +13,6 @@ import nz.ac.auckland.se206.SceneManager;
 /** Controller class for the room view. */
 public class RoomController {
 
-  private BooleanProperty wPressed = new SimpleBooleanProperty();
-  private BooleanProperty aPressed = new SimpleBooleanProperty();
-  private BooleanProperty sPressed = new SimpleBooleanProperty();
-  private BooleanProperty dPressed = new SimpleBooleanProperty();
-
-  private BooleanBinding keyPressed = wPressed.or(aPressed).or(sPressed).or(dPressed);
-
-  private int movementSpeed = 2;
-
-  @FXML
-  private Rectangle player;
-  @FXML
-  private Rectangle collider;
-
-  @FXML
-  private Pane room;
-
   @FXML
   private Rectangle door;
   @FXML
@@ -45,90 +20,9 @@ public class RoomController {
   @FXML
   private Rectangle vase;
 
-  AnimationTimer playerTimer = new AnimationTimer() {
-    @Override
-    public void handle(long timestamp) {
-      if (wPressed.get()) {
-        player.setY(player.getY() - movementSpeed);
-      }
-
-      if (aPressed.get()) {
-        player.setX(player.getX() - movementSpeed);
-      }
-
-      if (sPressed.get()) {
-        player.setY(player.getY() + movementSpeed);
-      }
-
-      if (dPressed.get()) {
-        player.setX(player.getX() + movementSpeed);
-      }
-    }
-  };
-
-  AnimationTimer collisionTimer = new AnimationTimer() {
-    @Override
-    public void handle(long timestamp) {
-      checkCollision(player, collider);
-    }
-  };
-
   /** Initializes the room view, it is called when the room loads. */
   public void initialize() {
     // Initialization code goes here
-    movementSetup();
-
-    keyPressed.addListener((observable, aBoolean, t1) -> {
-      if (!aBoolean) {
-        playerTimer.start();
-        collisionTimer.start();
-      } else {
-        playerTimer.stop();
-        collisionTimer.stop();
-      }
-    });
-  }
-
-  public void movementSetup() {
-    room.setOnKeyPressed(e -> {
-      switch (e.getCode()) {
-        case W:
-          wPressed.set(true);
-          break;
-        case A:
-          aPressed.set(true);
-          break;
-        case S:
-          sPressed.set(true);
-          break;
-        case D:
-          dPressed.set(true);
-          break;
-      }
-    });
-
-    room.setOnKeyReleased(e -> {
-      switch (e.getCode()) {
-        case W:
-          wPressed.set(false);
-          break;
-        case A:
-          aPressed.set(false);
-          break;
-        case S:
-          sPressed.set(false);
-          break;
-        case D:
-          dPressed.set(false);
-          break;
-      }
-    });
-  }
-
-  private void checkCollision(Rectangle player, Rectangle colliderObject) {
-    if (player.getBoundsInParent().intersects(colliderObject.getBoundsInParent())) {
-      System.out.println("collisionROOM");
-    }
   }
 
   /**
