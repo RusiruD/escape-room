8e2e9657c992447a9d7a80e677f894c87d3537c5
Rusiru <rusiru3@gmail.com>
2023-09-04 00:46:23 +1200

    Feat: added choicebox for choosing difficulty of game

--- src/main/java/nz/ac/auckland/se206/controllers/StartScreenController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartScreenController.java
@@ -3,12 +3,13 @@ import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.input.MouseEvent;
 
 public class StartScreenController {
 
     @FXML
     private ChoiceBox<String> timerChoice;
+    @FXML
+    private ChoiceBox<String> difficultyChoice;
 
     @FXML
     private void initialize() {
@@ -16,13 +17,19 @@ public class StartScreenController {
         timerChoice.getItems().add("4 minutes");
         timerChoice.getItems().add("6 minutes");
         timerChoice.setValue("2 minutes");
+        difficultyChoice.getItems().add("Easy");
+        difficultyChoice.getItems().add("Medium");
+        difficultyChoice.getItems().add("Hard");
+        difficultyChoice.setValue("Easy");
 
         // You can perform additional initialization here if needed.
     }
     @FXML
   private void onStartGame(ActionEvent event) {
     String s=timerChoice.getValue();
+    String x=difficultyChoice.getValue();
     System.out.println(s);
+    System.out.println(x);
   }
 
 
