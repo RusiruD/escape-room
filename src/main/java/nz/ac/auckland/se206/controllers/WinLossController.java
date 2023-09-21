454661db33d730374a6c16a3ee50a2e0a7b1305a
Rusiru <rdha868@aucklanduni.ac.nz>
2023-09-21 21:40:32 +1200

    Feat: added fxid for buttons and labels

---
+++ src/main/java/nz/ac/auckland/se206/controllers/WinLossController.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class WinLossController {
+  @FXML private Label gameOutcome;
+  @FXML private Label gameOutcomeDescription;
+  @FXML private Button exitGame;
+  @FXML private Button replayGame;
+  @FXML private Button newGame;
+}
